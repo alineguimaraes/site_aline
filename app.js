ff918880e8d632d0bfb78c66cfd7da9ddb9a6dc1
alineguimaraes <contatoalinesouzaguimaraes@gmail.com>
2021-08-14 13:25:20 -0300

    adiciona parte do formulario
    Co-authored-by: ${laionAzeredo} <laion.azeredo@gmail.com>

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ const app = express()
 // use the express-static middleware
 app.use(express.static("public"))
 
+app.use(express.urlencoded())
+app.use(express.json())
+
 // define the first route
 app.get("/", function (req, res) {
   res.send("<h1>Hello World!</h1>")
+})
+
+app.post("/form", function (req, res) {
+  console.log('conteúdo do formulário: ', req.body)
+  res.sendFile('public/obrigado.html', { root : __dirname})
 })
 
 // start the server listening for requests
